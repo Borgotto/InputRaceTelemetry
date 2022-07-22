0dff7511d8a166128adf06d854a684893545b6e6
Nicola Montanari <nicola.montanari14@studio.unibo.it>
2022-07-22 19:35:55 +0200

    Working Code!

--- GreatTest.py
+++ GreatTest.py
@@ -5,28 +5,46 @@ import time
 
 fig = plt.figure()
 
+ax = fig.add_subplot(1,1,1)
 
 screen = pf.screen(title='Plot')
 
+
+gas = np.zeros(100)
+
 start = time.time()
+count=0
 while True:
+    count+=1
     t = time.time() - start
-    x = np.linspace(t-3, t, 100)
+    x = np.linspace(0, 100, 100)
     steering = np.sin(2*np.pi*x) + np.sin(3*np.pi*x)
-    gas = np.sin(x)
+    
+    gas[count]=count
     brake = np.cos(x)
     clutch = np.cos(x)+2
 
-    plt.xlim(t-3,t)
-    plt.ylim(-5,5)
+    if(count==50):
+        gas[range(0,50)]=0
+    plt.cla()
+
+    #plt.xlim(t-3,t)
+    plt.ylim(0,100)
     plt.plot(x, steering, c='black') #nero
     plt.plot(x, gas, c='#16f747') #verde
     plt.plot(x, brake, c='blue') #rosso, blu!?!
     plt.plot(x, clutch, c='r') #blu, rosso!?!
     
-    plt.grid(False)
-    plt.axis('off')
+    frame = plt.gca()
+    frame.axes.get_yaxis().set_ticks([])
 
+    for i in range(10):
+        x = np.delete(x, i)
+        steering = np.delete(steering, i)
+
+    #plt.grid(False)
+    #plt.axis('off')
+    time.sleep(0.005)
     # If we haven't already shown or saved the plot, then we need to draw the figure first...
     fig.canvas.draw()
 
