fa53ec6d857f1d262bfb2522e35206840ae2c40b
Emanuele Borghini <manuborg2002@gmail.com>
2022-09-30 20:42:45 +0200

    Working GUI prototype

--- test-borgo.py
+++ test-borgo.py
@@ -1,59 +1,101 @@
 import os
 import sys
-from typing import Any, Tuple
 import irsdk
 import numpy as np
 import PySimpleGUI as sg
 import PIL.Image
+from typing import Any, Iterable, Tuple
 from io import BytesIO
 from time import sleep, time
 
 
-
-# Literal values
-BUFFER_SIZE = 100                           # Number of values to store in the buffer
-VALUE_RANGE = (0,100)                       # Range of values for the graphs
-LINE_WIDTH = 4                              # Graph line width
-UI_SCALING = 1                              # Resize the whole UI window
-FONT = ('Mont Heavy DEMO', 22, '#FFFFFF')   # This only changes the Gear and Speed text size (not the Throttle, Brake and Clutch graph values)
-
 class Value():
     """Class to store a value from the iRacing SDK and relative metadata for its rendering within the UI"""
-    def __init__(self, name: str, color: str = 'white', initial_value: Any = 0, type: np.dtype = np.int32, value_range: Tuple[Any,Any] = (0,100), buffer_size: int = 1):
-        self._name = name
-        self._color = color
-        self._initial_value = initial_value
-        self._type = type
-        self._value_range = value_range
-        self._buffer_size = buffer_size
-        self._values = np.full(self._buffer_size, max(min(max(self._value_range), self._initial_value), min(self._value_range)), dtype=self._type)
+    def __init__(self, name: str, color: str = 'white', range: Tuple[Any,Any] = (None,None), initial_value: Any = 0, type: np.dtype = np.int32, buffer_size: int = 1):
+        self._name = name.strip()
+        self._color = color.strip()
+        self._range = (min(range), max(range)) if range[0] and range[1] else range
+        self._values = np.full(abs(buffer_size or 1), self._clamp(initial_value), dtype=type)
 
-    def update(self, value: np.dtype):
-        """Shift the buffer and add the new value"""
-        self._value = np.roll(self._value, -1)
-        self._value[-1] = max(min(max(self._value_range), value), min(self._value_range))
+    def _clamp(self, value) -> np.dtype:
+        "Restrict a value to the range defined at initialization"
+        range = self.range
+        if range[1] is not None:
+            value = min(range[1], value)
+        if range[0] is not None:
+            value = max(range[0], value)
+        return value
 
-    def get(self) -> np.dtype:
-        """Return the last value in the buffer"""
-        return self._value[-1]
+    @property
+    def name(self) -> str:
+        """Return the name of the attribute"""
+        return self._name
 
-    def get_all_values(self) -> np.ndarray:
-        """Return the buffer containing all the values"""
-        return self._value
+    @property
+    def color(self) -> str:
+        """Return the color of the rendered graph lines"""
+        return self._color
 
-    def get_range(self) -> Tuple[Any,Any]:
+    @property
+    def buffer_size(self) -> int:
+        """Return the size of the buffer array"""
+        return self.values.size
+
+    @property
+    def is_buffered(self) -> bool:
+        """Check if the value has a buffer size greater than 1"""
+        return self.buffer_size > 1
+
+    @property
+    def range(self) -> Tuple[Any,Any]:
         """Return the value range"""
-        return self._value_range
+        return self._range
 
-values = {'Throttle': np.random.rand(BUFFER_SIZE)*50,
-            'Brake': np.zeros(BUFFER_SIZE, dtype=np.int32)+100,
-            'Clutch': np.zeros(BUFFER_SIZE, dtype=np.int32)+10,
-            'SteeringWheelAngle': np.zeros(BUFFER_SIZE, dtype=np.float32)
-            }
-colors = {'Throttle': 'red',
-            'Brake': 'blue',
-            'Clutch': 'green',
-            'SteeringWheelAngle': 'yellow'}
+    @property
+    def type(self) -> np.dtype:
+        """Return the data type of the value"""
+        return self.values.dtype
+
+    @property
+    def values(self) -> np.array:
+        """Return the buffer containing all the values"""
+        return self._values
+
+    @values.setter
+    def values(self, value: Iterable[Any]) -> None:
+        """Set n values in the buffer, extra values will be discarded"""
+        array = np.array([self._clamp(v) for v in value[-self.buffer_size:]], dtype=self.type)
+        self._values = np.roll(self.values, -array.size)
+        indices = np.linspace(self.buffer_size - array.size, self.buffer_size - 1, array.size, dtype=np.int32)
+        np.put(self.values, indices, array)
+
+    @property
+    def value(self) -> np.dtype:
+        """Return the last value in the buffer"""
+        return self.values[-1]
+
+    @value.setter
+    def value(self, value: np.dtype) -> None:
+        """Set the last value in the buffer"""
+        self.values = [value]
+
+
+# Literal values
+UI_SCALING = 1
+FONT = {'name': 'Mont Heavy DEMO',
+        'size': 22,
+        'color': '#FFFFFF'}
+
+# Tracked values
+_values = [Value('Throttle',           color='red', type=np.float32, buffer_size=100, range=(0,100)),
+            Value('Brake',              color='blue', type=np.float32, buffer_size=100, range=(0,100)),
+            Value('Clutch',             color='green', type=np.float32, buffer_size=100, range=(0,100)),
+            Value('SteeringWheelAngle', color='yellow', type=np.float32, buffer_size=100, range=(-180,180)),
+            Value('Speed',              initial_value=0, type=np.int16, range=(0, None)),
+            Value('Gear',               initial_value='N', type=np.str0),
+            Value('DisplayUnits',       initial_value="kph", type=np.object0)]
+values = {v.name: v for v in _values}
+columns: Tuple[Value, Value, Value] = (values['Throttle'], values['Brake'], values['Clutch'])
 
 # iRating SDK initialization
 ir = irsdk.IRSDK()
@@ -62,25 +104,25 @@ ir = irsdk.IRSDK()
 image_bg = PIL.Image.open("bg.png")
 image_wheel = PIL.Image.open("wheel.png")
 background_layout = [[sg.Image("bg.png", key='bg')]]
-top_layout = [[sg.Graph((524,193),(0,VALUE_RANGE[0]-1),(BUFFER_SIZE-1,VALUE_RANGE[1]+1), key='graph', pad=((76,0),(13,0))),
-                sg.Graph((44,198),(0,VALUE_RANGE[0]),(1,VALUE_RANGE[1]+22), key='graph2', pad=((17,0),(0,0))),
-                sg.Graph((44,198),(0,VALUE_RANGE[0]),(1,VALUE_RANGE[1]+22), key='graph3', pad=((3,0),(0,0))),
-                sg.Graph((44,198),(0,VALUE_RANGE[0]),(1,VALUE_RANGE[1]+22), key='graph4', pad=((2,0),(0,0))),
-                sg.Graph((70,178),(0,0),(70,178), key='text_overlay', pad=((3,0),(28,0))),
-                sg.Image("wheel.png", key='wheel', pad=((0,0),(0,0)))]]
+top_layout = [[sg.Graph((524,193),(0,-1),(min([v.buffer_size for v in values.values() if v.is_buffered])-1,100+1), key='graph', pad=((76,0),(13,0))),
+                sg.Graph((44,198),(0,0),(1,100+22), key='column1',      pad=((17,0),(0,0))),
+                sg.Graph((44,198),(0,0),(1,100+22), key='column2',      pad=((3,0),(0,0))),
+                sg.Graph((44,198),(0,0),(1,100+22), key='column3',      pad=((2,0),(0,0))),
+                sg.Graph((70,178),(0,0),(70,178),   key='text_overlay', pad=((3,0),(28,0))),
+                sg.Image("wheel.png",               key='wheel',        pad=((0,0),(0,0)))]]
 
 
 
 # Utility functions
-def make_borderless_window(layout, title):
+def _make_borderless_window(layout, title):
     """Create a borderless window given a layout and a title"""
     return sg.Window(title,layout,no_titlebar=True,keep_on_top=True,grab_anywhere=True,resizable=False,alpha_channel=0.8,transparent_color=sg.theme_background_color(),margins=(0, 0),element_padding=(0,0))
 
-def darken_color(color : str):
-    """Darken a color by 50% (str format should be '#RRGGBB')"""
-    return '#'+hex(int((int(color[1:],16)+int('6f6f6f',16))/2))[2:]
+def _darken_color(color : str):
+    """Darken a color by 50% (str format should be '#RRGGBB' or 'RRGGBB')"""
+    return '#'+hex(int((int(color[1:] if color.startswith('#') else color,16)+int('6f6f6f',16))/2))[2:]
 
-def image_to_bytes(image: PIL.Image):
+def _image_to_bytes(image: PIL.Image):
     """Convert a PIL.Image to bytes"""
     bio = BytesIO()
     image.save(bio, format="PNG")
@@ -88,37 +130,35 @@ def image_to_bytes(image: PIL.Image):
 
 def update_values():
     """Update dictionary values with the ones from iRacing"""
-    for k, v in values.items():
-        values[k] = np.roll(v, -1)
-        values[k][-1] = VALUE_RANGE[1]
-        values[k][-1] = abs( values[k][-2] + np.random.rand(1)*VALUE_RANGE[1]/4 - VALUE_RANGE[1]/8 ) % VALUE_RANGE[1]
-        #values[k][-1] = ir[k]
+    for v in values:
+        continue #TODO: remove continue once iracing integration is finished
+        v.value = ir[v.name]
 
 def update_gui(top_window : sg.Window):
     """Draw the new frame"""
-    # Erase previous frame
-    for graph in [top_window['graph'], top_window['graph2'], top_window['graph3'], top_window['graph4'], top_window['text_overlay']]:
-        graph.erase()
+    # Erase every graph's canvas
+    [e.erase() for e in top_window.AllKeysDict.values() if isinstance(e, sg.Graph)]
 
-    # Draw all values on main graph
+    # Draw all buffered values on the main graph
     graph: sg.Graph = top_window['graph']
-    for k, v in values.items():
-        graph.draw_lines([(i, v[i]) for i in range(v.size)], color=colors[k], width=LINE_WIDTH)
+    for v in [v for v in values.values() if v.is_buffered]:
+        graph.draw_lines([(i, v.values[i]) for i in range(v.buffer_size)], color=v.color, width=4)
 
-    # Draw Throttle, Brake and Clutch on their own graphs
-    for k, g in zip(['Throttle', 'Brake', 'Clutch'], [top_window['graph2'], top_window['graph3'], top_window['graph4']]):
-        g.draw_text(str(int(values[k][-1])), (g.TopRight[0]/2, (g.TopRight[1]-VALUE_RANGE[1])/2+VALUE_RANGE[1]), color=FONT[2] if int(values[k][-1]) == VALUE_RANGE[1] else darken_color(FONT[2]), font=(FONT[0], int(g.CanvasSize[0]/2.8)))
-        g.draw_line((g.TopRight[0]/2, 0), (g.TopRight[0]/2, values[k][-1]), color=colors[k], width=30)
+    # Draw Throttle, Brake and Clutch on their own column graphs
 
-    # Draw Gear and Speed on text_overlay graph
-    graph: sg.Graph = top_window['text_overlay']
-    graph.draw_text(str(int(np.random.rand()*10%7+1)), (1, graph.TopRight[1]), color=FONT[2], font=(FONT[0],int(FONT[1])+8), text_location=sg.TEXT_LOCATION_TOP_LEFT)
-    graph.draw_text("mph" if np.random.rand() > 0.5 else "kph", (0, FONT[1]+FONT[1]/2.8), color=FONT[2], font=(FONT[0],int(FONT[1]/1.4)), text_location=sg.TEXT_LOCATION_BOTTOM_LEFT)
-    graph.draw_text(str(int(np.random.rand()*200+1)), (FONT[1]*2, 0), color=FONT[2], font=(FONT[0],FONT[1]), text_location=sg.TEXT_LOCATION_BOTTOM_RIGHT)
+    for v, g in zip([values['Throttle'], values['Brake'], values['Clutch']], [top_window['column1'], top_window['column2'], top_window['column3']]):
+        g.draw_text(str(int(v.value)), (g.TopRight[0]/2, (g.TopRight[1]-v.range[1])/2+v.range[1]), color=FONT['color'] if int(v.value) >= v.range[1] else _darken_color(FONT['color']), font=(FONT['name'], int(g.CanvasSize[0]/2.8)))
+        g.draw_line((g.TopRight[0]/2, 0), (g.TopRight[0]/2, v.value), color=v.color, width=30)
+
+    # Draw Gear, Unit and Speed on text_overlay graph
+    graph = top_window['text_overlay']
+    graph.draw_text(str(values["Gear"].value), (1, graph.TopRight[1]), color=FONT['color'], font=(FONT['name'],int(FONT['size'])+8), text_location=sg.TEXT_LOCATION_TOP_LEFT)
+    graph.draw_text(str(values['DisplayUnits'].value), (0, FONT['size']+FONT['size']/2.8), color=FONT['color'], font=(FONT['name'],int(FONT['size']/1.4)), text_location=sg.TEXT_LOCATION_BOTTOM_LEFT)
+    graph.draw_text(str(values['Speed'].value), (FONT['size']*2, 0), color=FONT['color'], font=(FONT['name'],FONT['size']), text_location=sg.TEXT_LOCATION_BOTTOM_RIGHT)
 
     # Rotate wheel image
     image: sg.Image = top_window['wheel']
-    image.update(image_to_bytes(image_wheel.rotate(np.random.rand(1)*360)))
+    image.update(_image_to_bytes(image_wheel.rotate(values['SteeringWheelAngle'].value or 0)))
 
     # Render new frame
     top_window.refresh()
@@ -128,8 +168,8 @@ def update_gui(top_window : sg.Window):
 def main():
     # Setup GUI
     sg.Window._move_all_windows = True
-    background_window = make_borderless_window(background_layout, 'Background')
-    top_window = make_borderless_window(top_layout, 'Telemetry')
+    background_window = _make_borderless_window(background_layout, 'Background')
+    top_window = _make_borderless_window(top_layout, 'Telemetry')
     # Show windows
     background_window.finalize()
     top_window.finalize()
