5ff6e8ca3aa0ff1020c5c764d83f1d70eada9cbf
Emanuele Borghini <manuborg2002@gmail.com>
2022-10-09 14:06:18 +0200

    Moved Values inside main, changed sleep and debug print

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from value import Value
 
 DEBUG: bool = True # set to True to run without iRacing
 
+
 # Literal values
 UI_SCALE = 1
 LINE_WIDTH = 4
@@ -16,18 +17,6 @@ FONT = {'name': 'Mont Heavy DEMO',
         'size': 22,
         'color': '#FFFFFF'}
 
-# iRacing values to be tracked, buffered values will be rendered in a graph
-_values = [ Value('Throttle',           color='green',       type=float, range=(0.0,100.0),    buffer_size=50),
-            Value('Brake',              color='red',         type=float, range=(0.0,100.0),    buffer_size=50),
-            Value('Clutch',             color='blue',        type=float, range=(0.0,100.0),    buffer_size=50),
-            Value('SteeringWheelAngle', color='white',       type=float, range=(-180.0,180.0), buffer_size=50),
-            Value('Speed',              initial_value=0,     type=int,   range=(0, None)),
-            Value('Gear',               initial_value='N',   type=str),
-            Value('DisplayUnits',       initial_value="kph", type=str)]
-# Values dictionary
-values = {v.name: v for v in _values}
-# Tuple of 3 Values to be displayed in columns
-columns = (values['Throttle'], values['Brake'], values['Clutch'])
 
 def main():
     # Setup iRacing SDK
@@ -38,24 +27,34 @@ def main():
     sys.stdout = sys.__stdout__             # restore stdout
     print("Startup successful")
 
+    # iRacing values to be tracked, buffered values will be rendered in a graph
+    framerate = ir['FrameRate']
+    _values = [ Value('Throttle',           color='green',       type=float, range=(0.0,100.0),    buffer_size=framerate),
+                Value('Brake',              color='red',         type=float, range=(0.0,100.0),    buffer_size=framerate),
+                Value('Clutch',             color='blue',        type=float, range=(0.0,100.0),    buffer_size=framerate),
+                Value('SteeringWheelAngle', color='white',       type=float, range=(-180.0,180.0), buffer_size=framerate),
+                Value('Speed',              initial_value=0,     type=int,   range=(0, None)),
+                Value('Gear',               initial_value='N',   type=str),
+                Value('DisplayUnits',       initial_value="kph", type=str)]
+    # Values dictionary
+    values = {v.name: v for v in _values}
+    # Tuple of 3 Values to be displayed in columns
+    columns = (values['Throttle'], values['Brake'], values['Clutch'])
+
     # Setup GUI
     gui = Gui(UI_SCALE, LINE_WIDTH, FONT, TRANSPARENCY)
     gui.show()
 
     # Main loop
-    framerate = ir['FrameRate']
     while ir.is_connected:
-        start = time.time()                                # start frame calculation time
-        for v in _values: v.value = ir[v.name]             # update values from iRacing
-        gui.update(values, columns)                        # update GUI with new values
-        stop = time.time()                                 # stop frame time
-        time.sleep(max(1/framerate-(stop-start), 0))       # sleep until next frame
+        start = time.time()                                 # start frame calculation time
+        for v in _values: v.value = ir[v.name]              # update values from iRacing
+        gui.update(values, columns)                         # update GUI with new values
+        stop = time.time()                                  # stop frame time
+        time.sleep(1/framerate - time.time() % 1/framerate) # sleep until next frame
 
-        if DEBUG:                                          # DEBUG: print frame times
-            if round(stop-start,6) > round(1/framerate,6):
-                print(f"Frame took too long to render: {((stop-start)*1000):.4f}ms (should be < {((1/framerate)*1000):.4f}ms) - {(1/(stop-start)):.0f}fps")
-            else:
-                print(f"{((stop-start)*1000):.0f}ms - {(1/(stop-start)):.0f}fps")
+        if DEBUG:                                           # print debug info
+            print(f"{((stop-start)*1000):.3f}ms - {(1/(stop-start)):.0f}fps", f"- Frame took too long to render (should be < {((1/framerate)*1000):.3f}ms)" if round(stop-start,3) > round(1/framerate,3) else "")
     print("iRacing disconnected, exiting...")
 
 
