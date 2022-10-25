2d7f4c6acf53686dc90f36b2d36f4f575e5a73e3
Emanuele Borghini <manuborg2002@gmail.com>
2022-10-25 18:19:51 +0200

    Changed debug IRSDK class to its own file

---
+++ debugirsdk.py
@@ -0,0 +1,17 @@
+class IRSDK():
+    def __init__(self) -> None:
+        self.is_initialized = True
+        self.is_connected = True
+        self.FrameRate = 59.98
+        self.Throttle = 1.0
+        self.Brake = 0.93
+        self.Clutch = 0.75
+        self.SteeringWheelAngle = 3.333
+        self.Speed = 200
+        self.Gear = 0
+        self.DisplayUnits = 0
+        self.SteeringWheelAngleMax = 7.2
+    def startup(self):
+        return True
+    def __getitem__(self, key):
+        return getattr(self, key)
--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import sys
 import time
 import math
 
-from irsdk import IRSDK
 from gui import Gui
 from value import Value
 
-DEBUG: bool = True # set to True to run without iRacing
-
+DEBUG: bool = True # set to True to run without iRacing and print debug info
+if DEBUG:
+    from debugirsdk import IRSDK
+else:
+    from irsdk import IRSDK
 
 # Literal values
 UI_SCALE = 1
@@ -18,10 +20,9 @@ FONT = {'name': 'Mont Heavy DEMO',
         'size': 22,
         'color': '#FFFFFF'}
 
-
 def main():
     # Setup iRacing SDK
-    ir = IRSDK() if not DEBUG else type('irsdk.IRSDK', (object,), {'__getitem__': lambda self, value: getattr(self, value),'startup': lambda self: True,'is_initialized': True,'is_connected': True,'FrameRate': 59.98,'Throttle': 1.0,'Brake': 0.93,'Clutch': 0.75,'SteeringWheelAngle': 3.333,'Speed': 200,'Gear': 0,'DisplayUnits': 0,'SteeringWheelAngleMax': 7.2})()
+    ir = IRSDK()
     print("Waiting for iRacing...")
     sys.stdout = open(os.devnull, "w")      # suppress irsdk output
     while not ir.startup(): time.sleep(0.5) # wait for sdk to connect
@@ -59,7 +60,6 @@ def main():
         if DEBUG:                                           # print debug info
             print(f"{((stop-start)*1000):.3f}ms - {(1/(stop-start)):.0f}fps", f"- Frame took too long to render (should be < {((1/framerate)*1000):.3f}ms)" if round(stop-start,3) > round(1/framerate,3) else "")
     print("iRacing disconnected, exiting...")
-
 
 if __name__ == '__main__':
     main()
